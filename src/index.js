ad5b223ca2ce83f97573fef8b467cae9935895cf
theumee <33261293+theumee@users.noreply.github.com>
2023-05-28 11:03:52 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,10 +8,10 @@ const port = process.env.PORT || 3001;
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
+
 app.use(express.json());
 
 app.use(userRouter);
